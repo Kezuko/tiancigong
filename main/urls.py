87a926ca3011e72ce96c517be1eccf2b0d8d4b46
lusterless <kkqkkq16@gmail.com>
2023-08-23 23:38:07 +0800

    Added services redirection

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.homepage, name="homepage")
+    path("", views.homepage, name="homepage"),
+    path("<eng_name>/services/", views.services, name='services'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def homepage(request):
@@ -8,3 +9,9 @@ def homepage(request):
         template_name='main/base.html'
     )
 
+@login_required
+def services(request, eng_name):
+    return render(
+        request=request,
+        template_name='main/base.html'
+    )
