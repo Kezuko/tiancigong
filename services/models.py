f43acd6fba442c41d98462a22863703d83e84631
lusterless <kkqkkq16@gmail.com>
2023-09-01 14:36:10 +0800

    added order progress
    Missing order number and amend membership id format

--- services/models.py
+++ services/models.py
@@ -7,12 +7,20 @@ class Orders(models.Model):
         ('孔子', '孔子'),
         ('太岁', '太岁')
     )
+    progress = (
+        ("In Progress","In Progress"),
+        ("Completed","Completed")
+    )
     account = models.ForeignKey('users.AccountUser', on_delete=models.CASCADE)
     member = models.ForeignKey('users.Member', on_delete=models.CASCADE)
     order_type = models.CharField(max_length=255, choices=order_choices)
+    status = models.CharField(max_length=50, choices=progress, default="In Progress")
     
     def get_order_choices(self):
         return self.order_choices
+        
+    def get_progress(self):
+        return self.progress
     
     def __str__(self):
         return self.order_type
